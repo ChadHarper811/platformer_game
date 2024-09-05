54dc4652506cf5d76b5ebcdc5b70514e046d57e3
Chad Harper <chadharper811@gmail.com>
2024-09-04 21:32:32 -0500

    added class player with constructor

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = size => {
+    return innerHeight < 500 ? 
+    Math.ceil((size / 500) * innerHeight) :
+    size;
+};
+
+class Player {
+    constructor () {
+        this.position = {
+            x: proportionalSize(10),
+            y: proportionalSize(400),
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(40);
+    }
+    
+}
