8f71555cac17aec22f9bd41b4e23f19007bc9cfb
Chad Harper <chadharper811@gmail.com>
2024-09-09 22:16:04 -0500

    finished animate function adding keys for movement

--- script.js
+++ script.js
@@ -62,7 +62,10 @@ const player = new Player();
 const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0,0, canvas.width, canvas.height);
-};
+    player.update();
+}
+
+const keys = {}
 
 const startGame = () => {
     canvas.style.display = "block";
