d6d71c794d6815e3192a9bc5aa7f7f4e1adbbfb1
Chad Harper <chadharper811@gmail.com>
2024-09-09 22:34:10 -0500

    added if staement for no movement and switch for arrow keys

--- script.js
+++ script.js
@@ -78,7 +78,18 @@ const keys = {
 }
 
 const movePlayer = (key, xVelocity, isPressed) => {
-    
+    if (!isCheckpointCollisionDetectionActive) {
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        return;
+    }
+    switch (key) {
+        case "ArrowLeft":
+            keys.leftKey.pressed = isPressed;
+            if (xVelocity === 0) {
+                player.velocity.x = xVelocity
+            }
+    }
 }
 
 const startGame = () => {
