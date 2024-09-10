1b2e10408fc887af8504ae6149ab9d8ae3b7efd4
Chad Harper <chadharper811@gmail.com>
2024-09-09 22:53:57 -0500

    added constructor to platform class

--- script.js
+++ script.js
@@ -57,7 +57,16 @@ class Player {
     }
 }
 
-class Platform {} 
+class Platform {
+    constructor (x,y) {
+        this.position = {
+            x,
+            y,
+        };
+        this.width = 200;
+        this.height = proportionalSize(40);
+    }
+} 
 
 const player = new Player();
 
